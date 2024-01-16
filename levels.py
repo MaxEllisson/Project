775d10ed699b3e5ab3d68d4732e509454c8bd5ab
MaxEllisson <maxell0910@highgateschool.org.uk>
2024-01-16 12:24:54 +0000

    added load and remove function

--- levels.py
+++ levels.py
@@ -1,6 +1,6 @@
 import pymunk as pm
 import pygame as pg
-from spritess import Floor, Block, Ball
+from spritess import Floor, Block, Ball, Button
 from pymunk import Vec2d
 
 
@@ -11,12 +11,24 @@ class Level:
         self.space.gravity = (0, 981)
         self.shapes = pg.sprite.Group()
         self.weapons = pg.sprite.Group()
+        self.current_weapon = []
+        self.elements = pg.sprite.Group()
+        self.weapon_index = 0
 
-    def load_class(self):
+    def create_class(self):
         if self.game.class_choice == 1:
             self.weapons.add(Ball(self.game.display, Vec2d(100, 100), 50, self.space, 2, 0.5, 0.5))
+            self.weapons.add(Ball(self.game.display, Vec2d(100, 100), 20, self.space, 2, 0.5, 0.5))
+            self.weapons.add(Ball(self.game.display, Vec2d(100, 100), 10, self.space, 2, 0.5, 0.5))
+            self.load_weapon()
         else:
             self.weapons.add(Ball(self.game.display, Vec2d(100, 100), 50, self.space, 2, 0.5, 0.5))
+
+    def load_weapon(self):
+        self.current_weapon.append(self.weapons.sprites()[self.weapon_index])
+
+    def remove(self):
+        self.current_weapon.pop()
 
     def check_events(self):
         for event in pg.event.get():
@@ -26,10 +38,25 @@ class Level:
             if event.type == pg.KEYDOWN:
                 if event.key == pg.K_SPACE:
                     self.weapons.sprites()[0].launch(1000)
+            if event.type == pg.MOUSEBUTTONDOWN:
+                for element in self.elements:
+                    if isinstance(element, Button):
+                        if element.is_hovered():
+                            # match-case block for button function
+                            match element.low:
+                                case "weapon 1":
+                                    self.remove()
+                                    self.weapon_index = 0
+                                case "weapon 2":
+                                    self.remove()
+                                    self.weapon_index = 1
+                                case "weapon 3":
+                                    self.remove()
+                                    self.weapon_index = 2
 
     def run(self):
         self.game.in_game = True
-        self.load_class()
+        self.create_class()
         while self.game.in_game:
             self.check_events()
             self.game.display.fill('red')
@@ -38,6 +65,9 @@ class Level:
                     shapes.draw_block()
                 elif isinstance(shapes, Floor):
                     shapes.draw_floor()
+            for elements in self.elements:
+                if isinstance(elements, Button):
+                    elements.draw()
             for weapons in self.weapons:
                 if isinstance(weapons, Ball):
                     weapons.draw_ball()
@@ -51,5 +81,7 @@ class Level1(Level):
         super().__init__(game)
         floor = Floor(self.game.display, (0, 700), (1280, 700), self.space)
         block = Block(self.game.display, Vec2d(720, 510), (50, 400), self.space)
+        button = Button(self.game.display, (0, 0), (100, 100), 'weapon 1')
         # ball = Ball(self.game.display, Vec2d(100, 100), 50, self.space, 2, 0.5, 0.5)
         self.shapes.add(floor, block)
+        self.elements.add(button)
